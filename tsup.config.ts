5b776bbe6a2ed6b451572047a386b2dd264c1059
Nash Luis Maramag <09louisthebob@gmail.com>
2026-01-01 04:21:48 +0800

    Update tsup.config.ts

--- tsup.config.ts
+++ tsup.config.ts
@@ -4,8 +4,8 @@ const tsupConfig = defineConfig({
   // Entry points
   entry: {
     'cli/index': 'src/cli/index.ts',
-    'build/index': 'src/core/build/index.ts',   // temporary
-    'router/index': 'src/core/router/index.ts', // temporary
+    // 'build/index': 'src/core/build/index.ts',   // temporary
+    // 'router/index': 'src/core/router/index.ts', // temporary
   },
   
   // Output format
