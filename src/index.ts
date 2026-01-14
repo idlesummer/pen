6f4443fce5e801c16114a057348dea88834542fb
idlesummer <09louisthebob@gmail.com>
2026-01-14 23:23:53 +0800

    new

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { useRouter } from './core/router'
+export { App } from './core/runtime'
 export type { RouterContextValue } from './core/router'
 export type { ErrorComponentProps } from './core/runtime'
