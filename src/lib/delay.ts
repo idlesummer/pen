98778b7325b2d920ff15d72432eb39439077bc2c
idlesummer <09louisthebob@gmail.com>
2026-01-16 23:51:45 +0800

    Revert "Update delay.ts"
    This reverts commit 130e47975e537bfb99f01bf9761bceafc22fcfd0.

--- src/lib/delay.ts
+++ src/lib/delay.ts
@@ -1,5 +1,7 @@
+// src/lib/delay.ts
 /**
  * Wait for a specified number of milliseconds
+ * Useful for testing and demos
  */
 export function delay(ms: number): Promise<void> {
   return new Promise(resolve => setTimeout(resolve, ms))
