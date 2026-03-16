0730a66bb51b3c032f1b0f3fa698823b6840cabf
idlesummer <09louisthebob@gmail.com>
2026-03-17 06:49:00 +0800

    Update segment-tree.ts

--- src/pen/compiler/builders/segment-tree.ts
+++ src/pen/compiler/builders/segment-tree.ts
@@ -45,6 +45,7 @@ export function createSegmentTree(fileTree: FileNode): SegmentNode {
     attach: (child, parent) =>
       (parent.segmentNode.children!.push(child.segmentNode)),
   })
+
   return segmentTree
 }
 
