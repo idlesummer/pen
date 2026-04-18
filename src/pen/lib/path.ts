3b90b5818b773b93eb9ffce1409619908e139f1e
idlesummer <09louisthebob@gmail.com>
2026-04-18 19:20:36 +0800

    Update path.ts

--- src/pen/lib/path.ts
+++ src/pen/lib/path.ts
@@ -1,5 +1,11 @@
 import { parse, join } from 'path'
 
+/**
+ * Returns a file path without its extension.
+ *
+ * @example
+ * rootname('/foo/bar/baz.txt') // '/foo/bar/baz'
+ */
 export function rootname(filePath: string) {
   const { dir, name } = parse(filePath)
   return join(dir, name)
