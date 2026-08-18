7853ee3fc46dd5fa20ba4b46b35bc553e99b94ab
idlesummer <09louisthebob@gmail.com>
2026-08-18 22:50:12 +0800

    chore: renamed to CompileDiagnostic

--- src/pen/routing/router.ts
+++ src/pen/routing/router.ts
@@ -1,6 +1,6 @@
 import type { RouteNode } from './compiling/route-tree'
 import type { SearchNode } from './compiling/search-tree'
-import type { RouteIssue } from './compiling/validate'
+import type { CompileDiagnostic } from './compiling/validate'
 import type { Matcher } from './matcher'
 import { createCompiledRoutes } from './compiler'
 import { createMatcher } from './matcher'
@@ -9,7 +9,7 @@ export type Router = [
   match: Matcher,
   routeTree: RouteNode,
   searchTree: SearchNode,
-  routeIssues: RouteIssue[],
+  routeIssues: CompileDiagnostic[],
 ]
 
 export function createRouter(filePaths: string[]): Router {
