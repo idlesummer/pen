a0d119fbd400cdaf33c0916628eb9eaeb452d9bb
idlesummer <09louisthebob@gmail.com>
2026-08-24 04:20:52 +0800

    Update url.ts

--- src/pen/routing/matching/url.ts
+++ src/pen/routing/matching/url.ts
@@ -1,5 +1,7 @@
 export function normalizeUrl(urlString: string) {
   // should prepend a slash unless string is empty
   // should normalize multiple consecutive slashes into one slash
+  // guarantees:
+  // - no segment is empty string (safe to replace `str === undefined` to `!str`)
   return urlString.split('/')
 }
