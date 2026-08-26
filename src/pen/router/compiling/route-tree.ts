db95c3c749d7753e186a1d686fd0b0f80e51b01f
idlesummer <09louisthebob@gmail.com>
2026-08-27 06:12:11 +0800

    Update route-tree.ts

--- src/pen/router/compiling/route-tree.ts
+++ src/pen/router/compiling/route-tree.ts
@@ -85,3 +85,8 @@ export function forEachReachableRouteNode(root: RouteNode, visit: (routeNode: Ro
       routeNode.segment.type !== 'catchall' ? routeNode.children : [],
   })
 }
+
+/** Gets the route's source file or falls back to its route path if no module exists. */
+export function getDiagnosticPath(routeNode: RouteNode): string {
+  return Object.values(routeNode.modulePaths)[0] ?? routeNode.path
+}
