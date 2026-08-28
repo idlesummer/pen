1b2ecab692eb8360c33dd27a2f2ef7407763cefc
idlesummer <09louisthebob@gmail.com>
2026-08-29 02:41:45 +0800

    Update dict.ts

--- src/pen/lib/dict.ts
+++ src/pen/lib/dict.ts
@@ -1,7 +1,4 @@
-/** A string-keyed lookup object without `Object.prototype` properties. */
-export type Dict<T> = Record<string, T>
-
 /** Creates an empty `Dict` without `Object.prototype` properties. */
-export function dict<T>(): Dict<T> {
+export function dict<T>(): Record<string, T> {
   return Object.create(null)
 }
