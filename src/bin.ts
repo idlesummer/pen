4333e5e2e3d858adf1202d6ac8c19cbeb5ba3926
idlesummer <09louisthebob@gmail.com>
2026-08-29 22:56:17 +0800

    Update bin.ts

--- src/bin.ts
+++ src/bin.ts
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 import { defineCommand, runMain } from 'citty'
 import { CLI_NAME, DESCRIPTION, VERSION } from '@/lib/constants'
-import { buildCommand } from '@/react/cli/build-command'
+import { buildCommand } from '@/react/cli'
 
 const main = defineCommand({
   meta: {
