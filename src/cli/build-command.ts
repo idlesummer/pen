3e80ed7926394a9a26be92ae5e30a67e1faa31a1
idlesummer <09louisthebob@gmail.com>
2026-08-30 01:17:48 +0800

    Update build-command.ts

--- src/cli/build-command.ts
+++ src/cli/build-command.ts
@@ -1,13 +1,13 @@
 import { defineCommand } from 'citty'
 
+/** Creates a `build` CLI command that invokes the given build function. */
 export function defineBuildCommand<T>(build: () => T) {
   return defineCommand({
     meta: {
       name: 'build',
       description: 'Compile routes and generate static entry files for a pen app',
     },
-    run() {
-      build()
-    },
+    run: () =>
+      build(),
   })
 }
