7db2909c6b6aa812e679d7436f440f366ac65f61
idlesummer <09louisthebob@gmail.com>
2026-08-30 00:22:15 +0800

    Update build-command.ts

--- src/react/cli/build-command.ts
+++ src/react/cli/build-command.ts
@@ -1,6 +1,6 @@
 import { defineCommand } from 'citty'
-import { reportDiagnostics } from '@/lib/report-diagnostics'
-import { build } from '@/react/build'
+// import { reportDiagnostics } from '@/lib/report-diagnostics'
+// import { build } from '@/react/build'
 
 export const buildCommand = defineCommand({
   meta: {
@@ -8,10 +8,12 @@ export const buildCommand = defineCommand({
     description: 'Compile routes and generate static entry files for a pen app',
   },
   run: () => {
-    const { diagnostics } = build('app', 'dist')
-    reportDiagnostics(diagnostics)
+    // const { diagnostics } = build('app', 'dist')
+    // reportDiagnostics(diagnostics)
 
-    if (diagnostics.some(diagnostic => diagnostic.severity === 'error'))
-      process.exit(1)
+    // if (diagnostics.some(diagnostic => diagnostic.severity === 'error'))
+    //   process.exit(1)
+
+    console.log('Hello world from pen!')
   },
 })
