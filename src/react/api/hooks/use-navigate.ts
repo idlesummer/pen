80c3bd1461985a0ff5a21228b52cf5409141f066
idlesummer <09louisthebob@gmail.com>
2026-08-31 04:11:20 +0800

    Update use-navigate.ts

--- src/react/api/hooks/use-navigate.ts
+++ src/react/api/hooks/use-navigate.ts
@@ -3,7 +3,7 @@ import { NavigationContext } from '../NavigationProvider'
 
 export function useNavigate() {
   const store = use(NavigationContext)
-  if (!store) throw new Error('useNavigate must be used within a NavigationProvider')
-
+  if (!store)
+    throw new Error('useNavigate must be used within a NavigationProvider')
   return useSyncExternalStore(store.subscribe, store.getSnapshot)
 }
