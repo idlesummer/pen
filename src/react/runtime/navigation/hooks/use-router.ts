e1247e6decb0b847411ded952faba071f94fb3fd
idlesummer <09louisthebob@gmail.com>
2026-08-31 22:36:35 +0800

    Update use-router.ts

--- src/react/runtime/navigation/hooks/use-router.ts
+++ src/react/runtime/navigation/hooks/use-router.ts
@@ -3,7 +3,7 @@ import { NavigationContext } from '../NavigationProvider'
 
 export function useRouter() {
   const store = use(NavigationContext)
-  if (!store) throw new Error('useRouter must be used within a NavigationProvider')
-
+  if (!store)
+    throw new Error('useRouter must be used within a NavigationProvider')
   return store.actions
 }
