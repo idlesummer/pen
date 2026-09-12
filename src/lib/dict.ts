35b0330e83fc1bb31faf684790497faf5b836eaf
idlesummer <09louisthebob@gmail.com>
2026-09-12 12:32:16 +0800

    Update dict.ts

--- src/lib/dict.ts
+++ src/lib/dict.ts
@@ -2,3 +2,11 @@
 export function dict<T>(): Record<string, T> {
   return Object.create(null)
 }
+
+/** Returns whether a `Dict` has no entries.
+ *  Dicts have no prototype and are not modified through inheritance. */
+export function isEmpty<T>(dict: Record<string, T>): boolean {
+  for (const key in dict)
+    return false
+  return true
+}
