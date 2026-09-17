c68bb461077a61a8dfbf611b0aa4ea56e7a0dda8
idlesummer <09louisthebob@gmail.com>
2026-09-17 16:26:58 +0800

    Create json-indent.ts

---
+++ src/lib/json-indent.ts
@@ -0,0 +1,9 @@
+/**
+ * Adds a visible indentation guide for use with JSON.stringify.
+ *
+ * @param value The value to stringify.
+ * @returns Arguments for JSON.stringify with guided indentation.
+ */
+export function indent<T>(value: T): [T, null, string] {
+  return [value, null, '|  ']
+}
