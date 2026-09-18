c71c60fc3a6263f94bf2d9937f3cc7f3ab894619
idlesummer <09louisthebob@gmail.com>
2026-09-19 02:52:08 +0800

    Update render.tsx

--- src/react/runtime/renderer/render.tsx
+++ src/react/runtime/renderer/render.tsx
@@ -32,8 +32,7 @@ function wrapFrame(frame: Frame, content: ReactNode, params: Params, slotElement
     const Layout = resolveComponent('layout', layout, componentMap)
     const slotProps: Record<string, ReactNode> = {}
 
-    // doesn't run if slots is undefined
-    for (const name in slots)
+    for (const name in slots) // doesn't run if slots is undefined
       slotProps[name] = slotElements[name]
 
     const paramTable = sliceParams(params, paramDepth)
