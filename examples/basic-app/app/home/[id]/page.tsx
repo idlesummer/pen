15e7f04304c78601527a172497d0fa0258597534
idlesummer <09louisthebob@gmail.com>
2026-09-19 13:21:05 +0800

    Update page.tsx

--- examples/basic-app/app/home/[id]/page.tsx
+++ examples/basic-app/app/home/[id]/page.tsx
@@ -2,5 +2,10 @@ import { Text } from 'ink'
 import type { ParamTable } from '@idlesummer/pen'
 
 export default function HomeItemPage({ params }: { params: ParamTable }) {
-  return <Text>Hello World from HomeItemPage - id: {params.id}</Text>
+  return (
+    <>
+      <Text>Hello World from HomeItemPage - id: </Text>
+      <Text color="yellow">{params.id}</Text>
+    </>
+  )
 }
