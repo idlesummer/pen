236cece479d42db75c0f4ea185646af4623c4620
idlesummer <09louisthebob@gmail.com>
2026-09-23 14:59:08 +0800

    Update build.ts

--- src/cli/commands/build.ts
+++ src/cli/commands/build.ts
@@ -9,7 +9,7 @@ export const buildCommand = defineCommand({
   },
   run: async () => {
     const BUILD_APP_DIR = './src/app'
-    const BUILD_OUT_DIR = './pen/dist'
+    const BUILD_OUT_DIR = '.pen/dist'
     const diagnostics = await buildApp(BUILD_APP_DIR, BUILD_OUT_DIR)
 
     for (const { severity, text } of formatDiagnostics(diagnostics))
